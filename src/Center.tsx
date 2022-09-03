5b1b882c766ac28c79b10093d6d849faec16832b
George Bockari <bitwhys@gmail.com>
2022-09-02 23:45:12 -0400

    chore: initial commit

---
+++ src/Center.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+export type CenterProps = {
+  children: ReactNode
+}
+
+const Center = ({}: CenterProps) => {
+  return <div></div>
+}
+
+export default Center
